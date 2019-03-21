d5da70b1a210f7e49a3af69375745b127b14a34e
tikken <tikken23@gmail.com>
2019-03-22 00:30:03 +0300

    some

--- src/components/Banner/Banner.vue
+++ src/components/Banner/Banner.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="banner">
-  <img src="../../assets/banner/Oval.png" alt="" class="oval">
+  <img src="../../assets/banner/Oval.png" alt="Oval" class="oval">
   <img src="../../assets/banner/telephone.png" alt="" class="telephone">
 </div>
 </template>
